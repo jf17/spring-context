809ee266ea1e7d33f4fc8b749a2d58ffe7196b33
alex <javafirst17@gmail.com>
2023-11-14 14:41:49 +0300

    Add comment

--- src/main/java/ru/jf17/spring/context/JavaConfiguredCity.java
+++ src/main/java/ru/jf17/spring/context/JavaConfiguredCity.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.AnnotationConfigApplicationContext
 import ru.jf17.spring.context.city.City;
 import ru.jf17.spring.context.config.CityConfiguration;
 
+//Java only
 //recommended method
 public class JavaConfiguredCity {
     public static void main(String[] args) {
