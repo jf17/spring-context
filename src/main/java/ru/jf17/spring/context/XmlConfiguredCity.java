3b06d21cadc27cd20bf7fb0ec87f1db78314de71
alex <javafirst17@gmail.com>
2023-11-14 14:16:13 +0300

    Update XmlConfigured

--- src/main/java/ru/jf17/spring/context/XmlConfiguredCity.java
+++ src/main/java/ru/jf17/spring/context/XmlConfiguredCity.java
@@ -1,7 +1,14 @@
 package ru.jf17.spring.context;
 
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import ru.jf17.spring.context.city.City;
+
 public class XmlConfiguredCity {
     public static void main(String[] args) {
+        ApplicationContext applicationContext = new ClassPathXmlApplicationContext("city-context.xml");
+        City city = applicationContext.getBean(City.class);
+        System.out.println("city=" + city);
 
     }
 }
