8c8b69d846d7df3110161fb5369cf1d24909f501
alex <javafirst17@gmail.com>
2023-11-14 14:40:50 +0300

    Add comments

--- src/main/java/ru/jf17/spring/context/XmlConfiguredCity.java
+++ src/main/java/ru/jf17/spring/context/XmlConfiguredCity.java
@@ -4,6 +4,7 @@ import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 import ru.jf17.spring.context.city.City;
 
+// XML only
 public class XmlConfiguredCity {
     public static void main(String[] args) {
         ApplicationContext applicationContext = new ClassPathXmlApplicationContext("context.xml");
